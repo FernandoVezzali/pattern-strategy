491f5d9676c620c7d367137166dad9e7c0449d8c
Fernando Vezzali <Fernando.Vezzali@fenergo.com>
2016-06-10 18:01:31 +0100

    adding mvc project, unit tests and domain - before applying the patterns

---
+++ Before/Strategy.Domain/ShippingCostCalculatorService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy.Domain
+{
+    public class ShippingCostCalculatorService
+    {
+        public double CalculateShippingCost(Order order)
+        {
+            switch (order.ShippingMethod)
+            {
+                case Order.ShippingOptions.FedEx:
+                    return CalculateForFedEx(order);
+
+                case Order.ShippingOptions.UPS:
+                    return CalculateForUPS(order);
+
+                case Order.ShippingOptions.USPS:
+                    return CalculateForUSPS(order);
+
+                default:
+                    throw new UnknownOrderShippingMethodException();
+
+            }
+
+        }
+
+        double CalculateForUSPS(Order order)
+        {
+            return 3.00d;
+        }
+
+        double CalculateForUPS(Order order)
+        {
+            return 4.25d;
+        }
+
+        double CalculateForFedEx(Order order)
+        {
+            return 5.00d;
+        }
+    }
+}
---
+++ Before/Strategy.Tests/ShippingCostCalculatorService_Test.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Strategy.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy.Tests
+{
+    [TestClass]
+    public class ShippingCostCalculatorService_Test
+    {
+        [TestMethod]
+        public void When_shipping_via_UPS_The_shipping_cost_is_425()
+        {
+            var shippingCalculatorService = new ShippingCostCalculatorService();
+            var order = Mother.CreateOrder_UPS();
+            var cost = shippingCalculatorService.CalculateShippingCost(order);
+            Assert.AreEqual(4.25d, cost);
+        }
+
+        [TestMethod]
+        public void When_shipping_via_USPS_The_shipping_cost_is_300()
+        {
+            var shippingCalculatorService = new ShippingCostCalculatorService();
+            var order = Mother.CreateOrder_USPS();
+            var cost = shippingCalculatorService.CalculateShippingCost(order);
+            Assert.AreEqual(3.00d, cost);
+        }
+
+        [TestMethod]
+        public void When_shipping_via_FedEx_The_shipping_cost_is_5()
+        {
+            var shippingCalculatorService = new ShippingCostCalculatorService();
+            var order = Mother.CreateOrder_FedEx();
+            var cost = shippingCalculatorService.CalculateShippingCost(order);
+            Assert.AreEqual(5.00d, cost);
+        }
+    }
+}
