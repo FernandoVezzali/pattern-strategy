a91df8305dcba2ff632750b44f19b6b3e15c3957
FernandoVezzali <fernandovezzali@gmail.com>
2016-06-12 07:00:58 +0100

    adding viewmodel

--- Before/Strategy.Web/Controllers/HomeController.cs
+++ Before/Strategy.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Strategy.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,14 @@ namespace Strategy.Web.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            var order = new Order()
+            {
+                ShippingMethod = Order.ShippingOptions.FedEx,
+                Origin = new Address() { ContactName = "John Smith" }
+            };
+
+
+
             return View();
         }
     }
