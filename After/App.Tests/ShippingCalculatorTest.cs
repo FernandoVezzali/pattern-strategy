763cdad795ac763b2ac61ce9d0fe4a989e5aca11
Fernando Vezzali <fernandovezzali@gmail.com>
2016-06-18 03:11:42 +0100

    changing tests

--- After/App.Tests/ShippingCalculatorTest.cs
+++ After/App.Tests/ShippingCalculatorTest.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using App.Domain;
-using App.Domain.Strategy;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using App.Domain.Strategy;
 
 namespace App.Tests
 {
@@ -9,79 +9,73 @@ namespace App.Tests
     public class ShippingCalculatorTest
     {
         [TestMethod]
-        public void When_shipping_via_FedEx_The_shipping_cost_is_5()
+        public void When_Shipping_Via_FedEx_The_Cost_Is_5()
         {
             // Arrange
-            var shippingCalculator = new ShippingCalculator(new FedExStrategy());
             var order = new Order(ShippingOptions.FedEx);
 
             // Act
-            var cost = shippingCalculator. CalculateCost(order);
+            var cost = new ShippingCalculator(new FedExStrategy()).CalculateCost(order);
 
             Assert.AreEqual(5.00d, cost);
         }
 
         [TestMethod]
-        public void When_shipping_heavier_product_via_FedEx_The_shipping_cost_is_5()
+        public void When_Shipping_Heavier_Product_Via_FedEx_The_Cost_Is_55()
         {
             // Arrange
-            var shippingCalculator = new ShippingCalculator(new FedExStrategy());
             var order = new Order(ShippingOptions.FedEx, 310);
 
             // Act
-            var cost = shippingCalculator.CalculateCost(order);
+            var cost = new ShippingCalculator(new FedExStrategy()).CalculateCost(order);
 
             Assert.AreEqual(5.5d, cost);
         }
 
         [TestMethod]
-        public void When_shipping_via_UPS_The_shipping_cost_is_425()
+        public void When_Shipping_Via_UPS_The_Shipping_Cost_Is_425()
         {
             // Arrange
-            var shippingCalculator = new ShippingCalculator(new UpsStrategy());
             var order = new Order(ShippingOptions.UPS);
 
             // Act
-            var cost = shippingCalculator.CalculateCost(order);
+            var cost = new ShippingCalculator(new UpsStrategy()).CalculateCost(order);
 
             Assert.AreEqual(4.25d, cost);
         }
 
         [TestMethod]
-        public void When_shipping_heavier_product_via_UPS_The_shipping_cost_is_4675()
+        public void When_Shipping_Heavier_Product_Via_UPS_The_Cost_Is_4675()
         {
             // Arrange
-            var shippingCalculator = new ShippingCalculator(new UpsStrategy());
             var order = new Order(ShippingOptions.UPS, 410);
 
             // Act
-            var cost = shippingCalculator.CalculateCost(order);
+            var cost = new ShippingCalculator(new UpsStrategy()).CalculateCost(order);
 
             Assert.AreEqual(4.675d, Math.Round(cost, 3));
         }
 
         [TestMethod]
-        public void When_shipping_via_USPS_The_shipping_cost_is_300()
+        public void When_Shipping_Via_USPS_The_Cost_Is_300()
         {
             // Arrange
-            var shippingCalculator = new ShippingCalculator(new USPSStrategy());
             var order = new Order(ShippingOptions.USPS);
 
             // Act
-            var cost = shippingCalculator.CalculateCost(order);
+            var cost = new ShippingCalculator(new USPSStrategy()).CalculateCost(order);
 
             Assert.AreEqual(3.00d, cost);
         }
 
         [TestMethod]
-        public void When_shipping_book_via_USPS_The_shipping_cost_is_27()
+        public void When_Shipping_Book_Via_USPS_The_Cost_is_27()
         {
             // Arrange
-            var shippingCalculator = new ShippingCalculator(new USPSStrategy());
             var order = new Order(ShippingOptions.USPS, 0d, ProductType.Book);
 
             // Act
-            var cost = shippingCalculator.CalculateCost(order);
+            var cost = new ShippingCalculator(new USPSStrategy()).CalculateCost(order);
 
             Assert.AreEqual(2.7d, cost);
         }
--- Before/App.Tests/ShippingCalculatorTest.cs
+++ Before/App.Tests/ShippingCalculatorTest.cs
@@ -7,82 +7,74 @@ namespace App.Tests
     [TestClass]
     public class ShippingCalculatorTest
     {
-        private ShippingCalculator _shippingCalculator;
-
-        [TestInitialize]
-        public void TestInitialize()
-        {
-            _shippingCalculator = new ShippingCalculator();
-        }
-
         [TestMethod]
-        public void When_shipping_via_FedEx_The_shipping_cost_is_5()
+        public void When_Shipping_Via_FedEx_The_Cost_Is_5()
         {
             // Arrange
             var order = new Order(ShippingOptions.FedEx);
 
             // Act
-            var cost = _shippingCalculator.CalculateCost(order);
+            var cost = new ShippingCalculator().CalculateCost(order);
 
             Assert.AreEqual(5.00d, cost);
         }
 
         [TestMethod]
-        public void When_shipping_heavier_product_via_FedEx_The_shipping_cost_is_5()
+        public void When_Shipping_Heavier_Product_Via_FedEx_The_Cost_Is_55()
         {
             // Arrange
             var order = new Order(ShippingOptions.FedEx, 310);
 
             // Act
-            var cost = _shippingCalculator.CalculateCost(order);
+            var cost = new ShippingCalculator().CalculateCost(order);
 
             Assert.AreEqual(5.5d, cost);
         }
 
         [TestMethod]
-        public void When_shipping_via_UPS_The_shipping_cost_is_425()
+        public void When_Shipping_Via_UPS_The_Shipping_Cost_Is_425()
         {
             // Arrange
             var order = new Order(ShippingOptions.UPS);
 
             // Act
-            var cost = _shippingCalculator.CalculateCost(order);
+            var cost = new ShippingCalculator().CalculateCost(order);
 
             Assert.AreEqual(4.25d, cost);
         }
 
         [TestMethod]
-        public void When_shipping_heavier_product_via_UPS_The_shipping_cost_is_4675()
+        public void When_Shipping_Heavier_Product_Via_UPS_The_Cost_Is_4675()
         {
             // Arrange
             var order = new Order(ShippingOptions.UPS, 410);
 
             // Act
-            var cost = _shippingCalculator.CalculateCost(order);
+            var cost = new ShippingCalculator().CalculateCost(order);
 
             Assert.AreEqual(4.675d, Math.Round(cost, 3));
         }
 
         [TestMethod]
-        public void When_shipping_via_USPS_The_shipping_cost_is_300()
+        public void When_Shipping_Via_USPS_The_Cost_Is_300()
         {
             // Arrange
             var order = new Order(ShippingOptions.USPS);
 
             // Act
-            var cost = _shippingCalculator.CalculateCost(order);
+            var cost = new ShippingCalculator().CalculateCost(order);
 
             Assert.AreEqual(3.00d, cost);
         }
 
         [TestMethod]
-        public void When_shipping_book_via_USPS_The_shipping_cost_is_27()
+        public void When_Shipping_Book_Via_USPS_The_Cost_is_27()
         {
             // Arrange
             var order = new Order(ShippingOptions.USPS, 0d, ProductType.Book);
 
             // Act
-            var cost = _shippingCalculator.CalculateCost(order);
+            var cost = new ShippingCalculator().CalculateCost(order);
 
             Assert.AreEqual(2.7d, cost);
         }
